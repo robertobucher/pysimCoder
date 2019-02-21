ffc2a69036b9f2f1ef0f4afbb7bbcc2997cbc8d3
bucher <bucher@debian.supsi.ch>
2019-02-21 06:41:04 +0100

    Added block for UDPtransmit

---
+++ resources/blocks/rcpBlk/UDPsocketTxBlk.py
@@ -0,0 +1,20 @@
+def UDPsocketTxBlk(pin, IP, port):
+    """
+
+    Call:   UDPsocketTxBlk(pin, IP, port)
+
+    Parameters
+    ----------
+       pin: connected input port(s)
+       IP : IP Addr
+       port :  Port
+
+    Returns
+    -------
+       blk: RCPblk
+
+    """
+
+    blk = RCPblk('UDPsocketTx', pin, [], [0,0], 1, [], [port, 0], IP)
+    return blk
+
