bc7d8c2a4ba00b8acd3380d741ece2a456eabdb1
Michal Lenc <michallenc@seznam.cz>
2021-08-02 22:44:31 +0200

    Math: add blocks for Park and Clarke transformations
    This commit adds new block for forward and inverse Clarke and Park
    transformations. Forward Clarke transformation can be used either with
    2 or 3 inputs. The output values are averaging when using 3 inputs so
    the noise on channels has less effect on the control application.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

---
+++ resources/blocks/rcpBlk/Math/clarke_forwardBlk.py
@@ -0,0 +1,25 @@
+from supsisim.RCPblk import RCPblk
+from scipy import size
+
+def clarke_forwardBlk(pin, pout):
+    """
+
+    Call:   clarke_forwardBlk(pin, pout)
+
+    Parameters
+    ----------
+       pin: connected input port(s)
+       pout: connected output port(s)
+
+    Returns
+    -------
+        blk  : RCPblk
+
+    """
+
+    if (size(pin) != 3 and size(pin) != 2):
+        raise ValueError("Forward Clarke transformation has to have 2 or 3 inputs, current number of inputs is (%i)" % (size(pin)))
+
+    blk = RCPblk('forward_clarke', pin, pout, [0,0], 1, [], [])
+    return blk
+
---
+++ resources/blocks/rcpBlk/Math/park_forwardBlk.py
@@ -0,0 +1,22 @@
+from supsisim.RCPblk import RCPblk
+from scipy import size
+
+def park_forwardBlk(pin, pout):
+    """
+
+    Call:   park_forwardBlk(pin, pout)
+
+    Parameters
+    ----------
+       pin: connected input port(s)
+       pout: connected output port(s)
+
+    Returns
+    -------
+        blk  : RCPblk
+
+    """
+
+    blk = RCPblk('forward_park', pin, pout, [0,0], 1, [], [])
+    return blk
+
