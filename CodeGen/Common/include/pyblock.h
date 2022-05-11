7e7358b1c3b06dccd2c2521c78b84c4bcf43d88d
Michal Lenc <michallenc@seznam.cz>
2022-05-11 12:04:23 +0200

    pyblock.h: add new structures for SHV support and enhance python_block
    Structures python_block_name_map and python_block_name_entry were added
    to support interaction with SHV tree. Morover existing structure
    python_block was enhanced to store names of double and integers
    parameters.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- CodeGen/Common/include/pyblock.h
+++ CodeGen/Common/include/pyblock.h
@@ -1,3 +1,6 @@
+#ifndef PYBLOCK_H
+#define PYBLOCK_H
+
 #define CG_INIT  1
 #define CG_OUT   2
 #define CG_STUPD 3
@@ -6,13 +9,34 @@
 typedef struct {
   int nin;             /* Number of inputs */
   int nout;            /* Number of outputs */
-  int * dimIn;       /* Port signal dimension */
-  int * dimOut;      /* Port signal dimension */
+  int * dimIn;         /* Port signal dimension */
+  int * dimOut;        /* Port signal dimension */
   int *nx;             /* Cont. and Discr states */
   void **u;            /* inputs */
   void **y;            /* outputs */
   double *realPar;     /* Real parameters */
+  int realParNum;      /* Number of real parameters */
   int *intPar;         /* Int parameters */
+  int intParNum;       /* Number of int parameters */
   char * str;          /* String */
   void * ptrPar;       /* Generic pointer */
-}python_block;
+  char **realParNames; /* Names of real parameters */
+  char **intParNames;  /* Names of integer parameter */
+} python_block;
+
+/* SHV related structures */
+
+typedef struct {
+  const char * block_name;  /* Name of the block */
+  int block_idx;            /* Index in python_block structure */
+  int system_inputs;        /* Block has system inputs */
+  int system_outputs;       /* Block has system outputs */
+} python_block_name_entry;
+
+typedef struct {
+  const python_block_name_entry * blocks;   /* Pointer to python_block_name_entry structure */
+  const python_block * block_structure;     /* Pointer to python_block structure */
+  int blocks_count;                         /* Number of blocks */
+} python_block_name_map;
+
+#endif /* PYBLOCK_H */
