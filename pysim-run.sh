0e49dd3f9df3fcd54b908ea2080ed27a405b9513
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2022-10-23 09:10:28 +0200

    Update pysim-run script to reflect toolbox locations change
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- pysim-run.sh
+++ pysim-run.sh
@@ -5,17 +5,9 @@ export PYSUPSICTRL="$( cd "$(dirname "$0")" ; pwd )"
 if [ -n "$PYTHONPATH" ] ; then
   PYTHONPATH=":$PYTHONPATH"
 fi
-export PYTHONPATH="$PYSUPSICTRL/resources/blocks/rcpBlk:$PYSUPSICTRL/toolbox$PYTHONPATH"
 
-if true ; then
-  #export PYTHONPATH="$PYSUPSICTRL/toolbox/supsisim/src:$PYSUPSICTRL/toolbox/supsictrl/src:$PYTHONPATH"
-  PYSUPSICTRL_LINKS="$HOME/Documents/PYTHON/toolbox"
-  mkdir -p "$PYSUPSICTRL_LINKS"
-  rm -f "$PYSUPSICTRL_LINKS/supsisim"
-  ln -s "$PYSUPSICTRL/toolbox/supsisim/src" "$PYSUPSICTRL_LINKS/supsisim"
-  rm -f "$PYSUPSICTRL_LINKS/supsictrl"
-  ln -s "$PYSUPSICTRL/toolbox/supsictrl/src" "$PYSUPSICTRL_LINKS/supsictrl"
-  export PYTHONPATH="$PYSUPSICTRL_LINKS:$PYTHONPATH"
-fi
+export PYTHONPATH=":$PYSUPSICTRL/resources/blocks/rcpBlk$PYTHONPATH"
+export PYTHONPATH=":$PYSUPSICTRL/toolbox/supsictrl$PYTHONPATH"
+export PYTHONPATH="$PYSUPSICTRL/toolbox/supsisim$PYTHONPATH"
 
 /usr/bin/python3 "$PYSUPSICTRL/BlockEditor/pysimCoder.py" "$@"
