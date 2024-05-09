20a1c70f8ddf349d07bfd3f3a4f2a39621b5378e
David Storek <storedav@fel.cvut.cz>
2024-05-09 14:56:36 +0200

    fix: client.py getting stduck on get_param

--- toolbox/supsisim/supsisim/client.py
+++ toolbox/supsisim/supsisim/client.py
@@ -1,49 +1,86 @@
 #!/usr/bin/env python3
-import argparse
 import asyncio
-import logging
+from threading import Thread
 from os import environ
 
-from shv import RpcUrl, SimpleClient, RpcError
+from shv import RpcUrl, SimpleClient, RpcError, SHVType
+
+
+def _start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+
+async def _disconnect_client(client: SimpleClient):
+    print("ASYNC Disconnecting to broker...")
+    await client.disconnect()
+
+async def _connect_client(user: str, addr: str, port: str, password: str) -> SimpleClient | None:
+    print("ASYNC Connecting to broker...")
+    url: RpcUrl = RpcUrl.parse("tcp://{}@{}:{}?password={}".format(user, addr, port, password))
+
+    print("Connection to SHV broker: ", url.to_url())
+    return await SimpleClient.connect(url)
+
+
+async def _get_parameter_value(client: SimpleClient, mount_point:str, device_id: str, item: str, paramName: str) -> SHVType|None:
+    callUrl = "{}/{}/blocks/{}/parameters/{}".format(mount_point, device_id, paramName, item)
+    print("Get param url: ", callUrl)
+    try:
+        result = await client.call(callUrl, "get") 
+        print("Read result: ", result)
+        return result
+    except RpcError as e:
+        print("Can't read parameter ", paramName)
+        print(e)
+        return None
+
+async def _set_parameter_value(client: SimpleClient, mount_point:str, device_id: str, item: str, paramName: str, paramVal: SHVType):
+    callUrl = "{}/{}/blocks/{}/parameters/{}".format(mount_point, device_id, paramName, item)
+    print("Set param url: ", callUrl)
+    try:
+        return await client.call(callUrl, "set", paramVal) 
+    except RpcError as e:
+        print("Can't set parameter ", paramName)
+        print(e)
 
 
 class BrokerConnection:
     def __init__(self):
+        self.asyncio_loop = asyncio.new_event_loop()
+        self.iothread = loop = asyncio.new_event_loop()
+        self.connection: SimpleClient | None = None
         self.addr: str | None = None
         self.port: str | None = None
         self.user: str | None = None
         self.password: str | None = None
         self.device_id: str | None = None
         self.mount_point: str | None = None
-        self.connection: SimpleClient | None = None
 
-
-    async def _disconnect_client(self):
-        await self.client.disconnect()
-
-        self.client = None
-
-    async def _connect_client(self):
-        url: RpcUrl = RpcUrl.parse("tcp://{}@{}:{}?password={}".format(self.user, self.addr, self.port, self.password))
-
-        print("Connection to SHV broker: ", url.to_url())
-        self.connection = await SimpleClient.connect(url)
-
+        t = Thread(target=_start_background_loop, args=(self.asyncio_loop,), daemon=True)
+        t.start()
+        
+    
     def _connect(self):
-        asyncio.run(self._connect_client())
+        print("Connecting to broker...")
+        connection = asyncio.run_coroutine_threadsafe(_connect_client(self.user, self.addr, self.port, self.password), self.asyncio_loop).result()
+
+        if connection is None:
+            raise RuntimeError("Error while connecting to broker.")
+        
+
+        self.connection = connection
 
         print("Connected to broker.")
 
-        if self.connection is None:
-            raise RuntimeError("Error while connecting to broker.")
-
     def _disconnect(self):
-        asyncio.run(self._disconnect_client())
+        print("Disconnecting from broker...")
+        asyncio.run_coroutine_threadsafe(_disconnect_client(self.client), self.asyncio_loop).result()
 
-        if self.connection is not None:
-            raise RuntimeError("Could not disconnect from broker.")
+        self.connection = None
+        
+        print("Disconnected from broker.")
 
-    def _getClient(self):
+    def _get_connection(self):
         if self.connection is None:
             self._connect()
 
@@ -66,113 +103,31 @@ class BrokerConnection:
         self.device_id = device_id
         self.mount_point = mount_point
 
+        print("Updating connection parameters")
         if are_values_updated and self.connection is not None:
+            print("Disconnecting...")
             self._disconnect()
 
         if self.connection is None:
+            print("Connecting...")
             self._connect()
 
-    async def _getParameterValue(self, item: str, paramName: str):
-        callUrl = "{}/{}/blocks/{}/parameters/{}".format(self.mount_point, self.device_id, paramName, item)
-        print("Get param url: ", callUrl)
-        try:
-            client = self._getClient()
-            result = await client.call(callUrl, "get") 
-            print("Read result: ", result)
-            return result
-        except RpcError:
-            self._disconnect_client()
-            self.connection = None
+    def getParameterValue(self, item: str, paramName: str) -> SHVType|None:
+        print("GetParam get connection")
+        client = self._get_connection()
+        print("GetParam call getParam async..")
+        return asyncio.run_coroutine_threadsafe(
+            _get_parameter_value(client, self.mount_point, self.device_id, item, paramName), 
+            self.asyncio_loop
+            ).result()
 
-    async def _setParameterValue(self, item: str, paramName: str, paramVal):
-        callUrl = "{}/{}/blocks/{}/parameters/{}".format(self.mount_point, self.device_id, paramName, item)
-        print("Set param url: ", callUrl)
-        try:
-            client = self._getClient()
-            return await client.call(callUrl, "set", paramVal) 
-        except RpcError:
-            self._disconnect_client()
-            self.connection = None
-
-
-    # async def _exampleStuff(self, item, name):
-    #     callUrl = "{}/{}/blocks/{}/parameters/{}".format(self.mount_point, self.device_id, name, item)
-    #     print("Example stuf, callUrls: ", callUrl)
-    #     res = await self._getClient().call(callUrl, "get") 
-    
-    #     print(f"Gain received: {repr(res)}")
-    #     return res
-
-    def getParameterValue(self, item: str, paramName: str):
-        return asyncio.run(self._getParameterValue(item, paramName))
-
-    def setPrameterValue(self, item: str, paramName: str, paramVal):
-        asyncio.run(self._setParameterValue(item, paramName, paramVal))
+    def setPrameterValue(self, item: str, paramName: str, paramVal: SHVType):
+        client = self._get_connection()
+        asyncio.run_coroutine_threadsafe(
+            _set_parameter_value(client, self.mount_point, self.device_id, item, paramName, paramVal), 
+            self.asyncio_loop
+            ).result()
 
     def doExampleStuff(self, item, name):
-        print("Do example stuff")
-        # asyncio.run(self._exampleStuff(item, name))
-        returnedVal = self.getParameterValue(item, name)
-        print("Example returned val: ", returnedVal)
-
-
-
-
-log_levels = (
-    logging.DEBUG,
-    logging.INFO,
-    logging.WARNING,
-    logging.ERROR,
-    logging.CRITICAL,
-)
-
-
-async def example_client(url: RpcUrl, item, name) -> None:
-    print("Example client url: ", url.to_url())
-
-    client = await SimpleClient.connect(url)
-    assert client is not None
-
-    callUrl="test/myDeviceId/blocks/{}/parameters/{}".format(name, item)
-    print("Example stuff call url: ", callUrl)
-    res = await client.call(callUrl, "get") 
-    
-    print(f"Gain received: {repr(res)}")
-    gain = int(res) + 10
-    # res = await client.call("test/test_pyshv/blocks/{}/parameters/{}".format(name, item), "set", gain)
-    # print(f"Gain set: {repr(res)}")
-    await client.disconnect()
-    return res
-
-
-def parse_args(address, password) -> argparse.Namespace:
-    """Parse passed arguments and return result."""
-    parser = argparse.ArgumentParser(description="Silicon Heaven example client")
-    parser.add_argument(
-        "-v",
-        action="count",
-        default=0,
-        help="Increase verbosity level of logging",
-    )
-    parser.add_argument(
-        "-q",
-        action="count",
-        default=0,
-        help="Decrease verbosity level of logging",
-    )
-    parser.add_argument(
-        "URL",
-        nargs="?",
-        default="tcp://admin@{}?password={}".format(address, password),
-        help="SHV RPC URL specifying connection to the broker.",
-    )
-    return parser.parse_args()
-
-
-def run_client(address, pas, items, name):
-    args = parse_args(address, pas)
-    logging.basicConfig(
-        level=log_levels[sorted([1 - args.v + args.q, 0, len(log_levels) - 1])[1]],
-        format="[%(asctime)s] [%(levelname)s] - %(message)s",
-    )
-    return asyncio.run(example_client(RpcUrl.parse(args.URL), items, name))
+        print("Do example stuff")  
+        self.getParameterValue(item, name)
