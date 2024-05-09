119d511e93b433f14380859e38bf474395963608
David Storek <storedav@fel.cvut.cz>
2024-05-09 15:47:00 +0200

    feat: gracefull client shutdowm

--- toolbox/supsisim/supsisim/client.py
+++ toolbox/supsisim/supsisim/client.py
@@ -7,15 +7,19 @@ from shv import RpcUrl, SimpleClient, RpcError, SHVType
 
 
 def _start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
-        asyncio.set_event_loop(loop)
+    asyncio.set_event_loop(loop)
+    try:
         loop.run_forever()
+    finally:
+        for task in asyncio.all_tasks():
+            task.cancel()
+        
+    loop.close()
 
 async def _disconnect_client(client: SimpleClient):
-    print("ASYNC Disconnecting to broker...")
     await client.disconnect()
 
 async def _connect_client(user: str, addr: str, port: str, password: str) -> SimpleClient | None:
-    print("ASYNC Connecting to broker...")
     url: RpcUrl = RpcUrl.parse("tcp://{}@{}:{}?password={}".format(user, addr, port, password))
 
     print("Connection to SHV broker: ", url.to_url())
@@ -47,7 +51,7 @@ async def _set_parameter_value(client: SimpleClient, mount_point:str, device_id:
 class BrokerConnection:
     def __init__(self):
         self.asyncio_loop = asyncio.new_event_loop()
-        self.iothread = loop = asyncio.new_event_loop()
+        self.asyncio_thread = Thread(target=_start_background_loop, args=(self.asyncio_loop,), daemon=True)
         self.connection: SimpleClient | None = None
         self.addr: str | None = None
         self.port: str | None = None
@@ -56,8 +60,14 @@ class BrokerConnection:
         self.device_id: str | None = None
         self.mount_point: str | None = None
 
-        t = Thread(target=_start_background_loop, args=(self.asyncio_loop,), daemon=True)
-        t.start()
+        self.asyncio_thread.start()
+
+    def __del__(self):
+        if self.connection is not None:
+            self._disconnect()
+            self.connection = None
+
+        self.asyncio_loop.stop()
         
     
     def _connect(self):
@@ -103,19 +113,14 @@ class BrokerConnection:
         self.device_id = device_id
         self.mount_point = mount_point
 
-        print("Updating connection parameters")
         if are_values_updated and self.connection is not None:
-            print("Disconnecting...")
             self._disconnect()
 
         if self.connection is None:
-            print("Connecting...")
             self._connect()
 
     def getParameterValue(self, item: str, paramName: str) -> SHVType|None:
-        print("GetParam get connection")
         client = self._get_connection()
-        print("GetParam call getParam async..")
         return asyncio.run_coroutine_threadsafe(
             _get_parameter_value(client, self.mount_point, self.device_id, item, paramName), 
             self.asyncio_loop
@@ -127,7 +132,3 @@ class BrokerConnection:
             _set_parameter_value(client, self.mount_point, self.device_id, item, paramName, paramVal), 
             self.asyncio_loop
             ).result()
-
-    def doExampleStuff(self, item, name):
-        print("Do example stuff")  
-        self.getParameterValue(item, name)
