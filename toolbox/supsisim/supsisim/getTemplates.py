326f83e2a77372a866f805f918f0c5f5e1a2ef82
Roberto Bucher <roberto.bucher@sunrise.ch>
2025-01-30 15:29:48 +0100

    Eliminated from list the not yet working algorithms

--- toolbox/supsisim/supsisim/getTemplates.py
+++ toolbox/supsisim/supsisim/getTemplates.py
@@ -1,7 +1,6 @@
 fixed_step =  ['gsl_odeiv2_step_rk2', 'gsl_odeiv2_step_rk4' ]
 variable_step = ['gsl_odeiv2_step_rkf45', 'gsl_odeiv2_step_rkck', 'gsl_odeiv2_step_rk8pd', \
-                   'gsl_odeiv2_step_rk2imp', 'gsl_odeiv2_step_rk4imp', 'gsl_odeiv2_step_bsimp', 'gsl_odeiv2_step_rk1imp', \
-                   'gsl_odeiv2_step_msadams', 'gsl_odeiv2_step_msbdf']
+                   'gsl_odeiv2_step_msadams']
 embedded =  ['standard RK4']
 
 dictTemplates = {
