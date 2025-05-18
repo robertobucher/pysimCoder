1159abb3d7627093db4f98507163751e46652d52
Michal Lenc <michallenc@seznam.cz>
2025-05-18 16:57:00 +0200

    pysimCoder.py: add argument to generate C code from command line
    This adds optional argument -g. If true, pysimCoder GUI is not opened,
    but the code is generation is performed. This argument has sense only
    if valid diagram file name is passed as a optional positional argument
    FILENAME.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- BlockEditor/pysimCoder.py
+++ BlockEditor/pysimCoder.py
@@ -1,48 +1,75 @@
 #!/usr/bin/python3
 
-import sys
+import argparse
 import os
-
-from supsisim.qtvers import *
+import sys
 
 from supsisim.library import Library
+from supsisim.pyEdit import NewEditorMainWindow
+from supsisim.qtvers import *
+
+
+def parse_args() -> tuple[argparse.Namespace, list]:
+    """Parse passed arguments and return result."""
+    parser = argparse.ArgumentParser("pysimCoder", description="pysimCoder")
+    parser.add_argument(
+        "-g",
+        "--generate",
+        action="store_true",
+        help="Generate the code without opening diagram",
+    )
+    parser.add_argument(
+        "FILENAME",
+        nargs="?",
+        default=None,
+        help="Name of the file to be opened. Leaving this argument opens empty diagram.",
+    )
+    return parser.parse_known_args()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        if filename[-4:] != '.dgm':
-            filename = filename + '.dgm'
+    args, unknown = parse_args()
+    filename = args.FILENAME
+
+    if filename is not None:
+        if filename[-4:] != ".dgm":
+            filename = filename + ".dgm"
         if not os.path.isfile(filename):
-            filename = 'untitled'
+            filename = "untitled"
     else:
-        filename = 'untitled'
+        filename = "untitled"
 
-    if filename!='untitled':
-        fname = QFileInfo(filename)
-        mypath = str(fname.absolutePath())
-        fname = str(fname.baseName())
+    if args.generate:
+        if filename == "untitled":
+            raise NameError("You must enter a valid pysimCoder digram file")
+        app = QApplication(unknown)
+
+        fname = str(QFileInfo(filename).baseName())
+        main = NewEditorMainWindow(fname, QFileInfo(filename).absolutePath(), None)
+        ret = main.codegenAct()
+        app.deleteLater()
+        os.remove("tmp.py")
+        if not ret:
+            sys.exit("Failed to generate C code")
     else:
-        fname = 'untitled'
-        mypath =  os.getcwd()
+        app = QApplication(unknown)
+        screen = app.primaryScreen()
+        rect = screen.availableGeometry()
+        h = int(rect.height() * 0.9)
 
-    app = QApplication(sys.argv)
-    screen = app.primaryScreen()
-    rect = screen.availableGeometry()
-    h = int(rect.height()*0.9)
-    
-    library = Library()
-    library.setGeometry(0, 0, 400, h)
-    library.setMaximumWidth(800)
-    library.setMinimumWidth(400)
-    library.show()
-    library.setWindowState(Qt.WindowState.WindowMaximized)
-       
-    if fname == 'untitled':
-        library.newFile()
+        library = Library()
+        library.setGeometry(0, 0, 400, h)
+        library.setMaximumWidth(800)
+        library.setMinimumWidth(400)
+        library.show()
         library.setWindowState(Qt.WindowState.WindowMaximized)
-    else:
-        library.fopen(fname)
-        library.setWindowState(Qt.WindowState.WindowMinimized)
-    ret = app.exec()
-    app.deleteLater()
+
+        if filename == "untitled":
+            library.newFile()
+            library.setWindowState(Qt.WindowState.WindowMaximized)
+        else:
+            library.fopen(filename)
+            library.setWindowState(Qt.WindowState.WindowMinimized)
+        ret = app.exec()
+        app.deleteLater()
 
